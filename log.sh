bc2c726f59aa75ee40e728f7e33b9033a0107010
Molik, David <dmolik@cshl.edu>
2015-07-01 10:55:32 -0400

    updated log.sh

--- log.sh
+++ log.sh
@@ -3,8 +3,8 @@
 log_check () {
 # doesn't take any arguments, just checks to make sure that logging is going to work, or its off. 
 	if [ "$logging" = "on" ]; then
-		[ -d "$log_file" ] || echo "error: log directory \"$log_file\" (log files) not found, quiting"; break
-		[ -d "$error_file" ] || echo "error: log directory \"$error_file\" (error files) not found, quiting"; break
+		if [ -d "$log_file" ]; then "error: log directory \"$log_file\" (log files) not found, quiting"; exit; fi
+		if [ -d "$error_file" ]; then "error: log directory \"$error_file\" (error files) not found, quiting"; exit; fi; 
 	elif [ -z "$logging" ]; then
 		echo "ok... no logging then"
 	else
@@ -14,11 +14,11 @@ log_check () {
 }
 
 post_to_error () {
-	[ "$logging" = "on" ] &&  echo "[$(date)]: $1" >> "$log_file/latest_sauce_$(date +%e_%m_%Y)"
+	[ "$logging" = "on" ] &&  echo "[$(date)]: $1" >> "$error_file"
 	[ -z "$logging" ] &&  echo "[$(date)]: $1"
 }
 
 post_to_log () {
-	[ "$logging" = "on" ] && echo "[$(date)]: $1" >> "$error_file/latest_sauce_err_$(date +%e_%m_%Y)"
+	[ "$logging" = "on" ] && echo "[$(date)]: $1" >> "$log_file"
 	[ -z "$logging" ] &&  echo "[$(date)]: $1"
 }
